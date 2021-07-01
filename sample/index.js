bbd491b49541530f27e1707044d2ed0e37f6d64b
Marlon Macedo <marlonsantosmacedo@outlook.com>
2021-06-30 22:14:42 -0300

    Removed unused code from sample

--- sample/index.js
+++ sample/index.js
@@ -1,17 +1,10 @@
 import { Person } from "./Person.js";
 import { Debug } from "mosaic/diagnostics";
-import { a, get, set } from "./variable.js";
 
 // Instantiate me! :3
 const person = new Person(
 	"Marlon Macedo",
 	new Date(1999, 11, 20, 23, 58) // Month is index-based
 );
-
-Debug.log(Debug.constructor.toString());
-
-Debug.log("b");
-const dbg2 = Debug.constructor();
-dbg2.log(dbg2.constructor.toString());
 
 Debug.log(person.toString());
